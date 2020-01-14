01ab01cbb992216507e9ac7785b5e0a6c15115aa
Andrew Moskalets <MoskaletsAndrey@yandex.ru>
2020-01-14 11:17:42 +0300

    stack сделан

---
+++ src/lesson4/MyLinkedStack.java
@@ -0,0 +1,26 @@
+package lesson4;
+
+public class MyLinkedStack<T>  {
+    private MyLinkedList<T> stack = new MyLinkedList<>();
+
+    public int size(){
+        return stack.size();
+    }
+
+    public boolean isEmpty(){
+        return stack.isEmpty();
+    }
+
+    public void push(T value) {
+        stack.insertLast(value);
+    }
+
+    public T pop() {
+        return stack.removeLast();
+    }
+
+    public T peek(){
+        return stack.getLast();
+    }
+
+}
